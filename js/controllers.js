719fd41bcaca8b80e5af56edc4a822d3fcfae96d
Nadim Tuhin <nadimtuhin@gmail.com>
2015-02-07 12:49:44 +0600

    fixed indentatino

--- js/controllers.js
+++ js/controllers.js
@@ -1,124 +1,124 @@
 angular.module('app.controllers', [])
 
 .controller("AppCtrl", ['$scope','$ionicPopover', '$ionicScrollDelegate',
-    function($scope, $ionicPopover, $ionicScrollDelegate){
+	function($scope, $ionicPopover, $ionicScrollDelegate){
 
-        //todo: save the settings in localStorage
-        $scope.medium = {
-            showArabic: true, 
-            showBangla: true,
-            reset: function(){
-                $scope.medium.showArabic = $scope.medium.showBangla = true;
-                $scope.closePopover();
-            }
-        }
+		//todo: save the settings in localStorage
+		$scope.medium = {
+			showArabic: true, 
+			showBangla: true,
+			reset: function(){
+				$scope.medium.showArabic = $scope.medium.showBangla = true;
+				$scope.closePopover();
+			}
+		}
 
-        $ionicPopover.fromTemplateUrl('moreMenu', {
-            scope: $scope,
-        }).then(function(popover) {
-            $scope.popover = popover;
-        });
+		$ionicPopover.fromTemplateUrl('moreMenu', {
+			scope: $scope,
+		}).then(function(popover) {
+			$scope.popover = popover;
+		});
 
-        $scope.openPopover = function($event) {
-            $scope.popover.show($event);
-        };
+		$scope.openPopover = function($event) {
+			$scope.popover.show($event);
+		};
 
-        $scope.closePopover = function() {
-            $scope.popover.hide();
-        };
+		$scope.closePopover = function() {
+			$scope.popover.hide();
+		};
 
-        $scope.scrollTop = function() {
-            $ionicScrollDelegate.scrollTop();
-        };
+		$scope.scrollTop = function() {
+			$ionicScrollDelegate.scrollTop();
+		};
 
-        $scope.scrollBottom = function() {
-            $ionicScrollDelegate.scrollBottom();
-        };
+		$scope.scrollBottom = function() {
+			$ionicScrollDelegate.scrollBottom();
+		};
 }])
 
 .controller("SurahCtrl", ['$scope','$stateParams','SurahService','BoxService','AudioSurahService', '$q',
-    function ($scope,$stateParams,SurahService,BoxService,AudioSurahService,$q) {
-        var surahBox = new BoxService(1000, 3); //load 3 lines each turn
-        var audio = document.getElementsByTagName('audio');
-        var surahLoaded = $q.defer(), audioLoading = $q.defer();
+	function ($scope,$stateParams,SurahService,BoxService,AudioSurahService,$q) {
+		var surahBox = new BoxService(1000, 3); //load 3 lines each turn
+		var audio = document.getElementsByTagName('audio');
+		var surahLoaded = $q.defer(), audioLoading = $q.defer();
 
-        $scope.bufstate = false;
-        $scope.ayahs = [];
-        
+		$scope.bufstate = false;
+		$scope.ayahs = [];
+		
 
-        //load surah from json
-        SurahService.loadSurah($stateParams.surahId).then(function(surah){
-            surahBox.setSource(surah.ayahs);
-            $scope.surah = surah;
+		//load surah from json
+		SurahService.loadSurah($stateParams.surahId).then(function(surah){
+			surahBox.setSource(surah.ayahs);
+			$scope.surah = surah;
 
-            surahLoaded.resolve();
-        });
+			surahLoaded.resolve();
+		});
 
-        //load audio stream after surah loaded
-        surahLoaded.promise.then(function(){
-            $scope.surah.audio_src = AudioSurahService.getSrc($stateParams.surahId);
-            
-            audio[0].addEventListener('error', function(e) {
-                alert("Unable to fetch data, please check your internet connection.")
-                $scope.bufstate = false;
-                console.log('bufstate is '+$scope.bufstate);
-            });
+		//load audio stream after surah loaded
+		surahLoaded.promise.then(function(){
+			$scope.surah.audio_src = AudioSurahService.getSrc($stateParams.surahId);
+			
+			audio[0].addEventListener('error', function(e) {
+				alert("Unable to fetch data, please check your internet connection.")
+				$scope.bufstate = false;
+				console.log('bufstate is '+$scope.bufstate);
+			});
 
-            audio[0].addEventListener('canplay', function() {
-                $scope.bufstate = false;
-                console.log('canplay .. bufstate is '+$scope.bufstate);
-                $scope.$apply();
-            });
-        });
-
-
-
-        $scope.play = function() {
-            audio[0].play();
-            console.log('bufstate is '+$scope.bufstate);
-            $scope.bufstate = true;
-        };
-        $scope.pause = function() {
-            audio[0].pause();
-        };
-        $scope.stop = function() {
-              $scope.pause();
-              audio[0].currentTime = 0;
-              console.log('bufstate is '+$scope.bufstate);
-        };
+			audio[0].addEventListener('canplay', function() {
+				$scope.bufstate = false;
+				console.log('canplay .. bufstate is '+$scope.bufstate);
+				$scope.$apply();
+			});
+		});
 
 
-        $scope.loadDown = function() {
-            console.log('loading down');
-            $scope.ayahs = surahBox.down();
-            $scope.$broadcast('scroll.infiniteScrollComplete');
-        };
 
-        $scope.canLoadDown = function(){
-            console.log('can we load more offscreen ayahs? ' + (surahBox.canLoadDown() ? "why not":"nope"));
-            return surahBox.canLoadDown();
-        };
+		$scope.play = function() {
+			audio[0].play();
+			console.log('bufstate is '+$scope.bufstate);
+			$scope.bufstate = true;
+		};
+		$scope.pause = function() {
+			audio[0].pause();
+		};
+		$scope.stop = function() {
+			  $scope.pause();
+			  audio[0].currentTime = 0;
+			  console.log('bufstate is '+$scope.bufstate);
+		};
+
+
+		$scope.loadDown = function() {
+			console.log('loading down');
+			$scope.ayahs = surahBox.down();
+			$scope.$broadcast('scroll.infiniteScrollComplete');
+		};
+
+		$scope.canLoadDown = function(){
+			console.log('can we load more offscreen ayahs? ' + (surahBox.canLoadDown() ? "why not":"nope"));
+			return surahBox.canLoadDown();
+		};
 
 }])
 
 .controller("SurahListCtrl", ['$scope','$stateParams','SurahListService','$ionicLoading', 'BoxService',
-    function ($scope,$stateParams,SurahListService,$ionicLoading, BoxService) {
-        var surahListBox = new BoxService(114, 10); //because we have 114 surahs, load 10 each time
-        $scope.surahs = [];
+	function ($scope,$stateParams,SurahListService,$ionicLoading, BoxService) {
+		var surahListBox = new BoxService(114, 10); //because we have 114 surahs, load 10 each time
+		$scope.surahs = [];
 
-        SurahListService.then(function(list){
-            surahListBox.setSource(list);
-        });
+		SurahListService.then(function(list){
+			surahListBox.setSource(list);
+		});
 
-        $scope.loadMore = function() {
-            console.log('loading more surahs');
-            $scope.surahs = surahListBox.down();
-            
-            $scope.$broadcast('scroll.infiniteScrollComplete');
-        };
+		$scope.loadMore = function() {
+			console.log('loading more surahs');
+			$scope.surahs = surahListBox.down();
+			
+			$scope.$broadcast('scroll.infiniteScrollComplete');
+		};
 
-        $scope.moreDataCanBeLoaded = function(){
-            console.log('can we load more surahs? ' + (surahListBox.canLoadDown() ? "why not":"nope"));
-            return surahListBox.canLoadDown() ;
-        };
+		$scope.moreDataCanBeLoaded = function(){
+			console.log('can we load more surahs? ' + (surahListBox.canLoadDown() ? "why not":"nope"));
+			return surahListBox.canLoadDown() ;
+		};
 }]);
--- js/services.js
+++ js/services.js
@@ -2,155 +2,155 @@ angular.module('app.services', [])
 
 //in future we will cache a few surahs so we need a dedicated service
 .factory('AudioSurahService', ['$sce', function($sce){
-    var getSrc = function(surahId){
-        var url = 'http://www.ourholyquran.com/surah/arabic/' + surahId + '.mp3';
-        // url = '1001.mp3';
-        return $sce.trustAsResourceUrl(url);
-    };
+	var getSrc = function(surahId){
+		var url = 'http://www.ourholyquran.com/surah/arabic/' + surahId + '.mp3';
+		// url = '1001.mp3';
+		return $sce.trustAsResourceUrl(url);
+	};
 
-    return {
-        getSrc: getSrc
-    };
+	return {
+		getSrc: getSrc
+	};
 }])
 
 
 .factory('SurahListService', ['$http', "$q", function($http, $q){
-    var deferred = $q.defer();
+	var deferred = $q.defer();
 
-    $http.get('/js/res/surah_list.json').then(function(res){
-        deferred.resolve(res.data);
-    });
+	$http.get('/js/res/surah_list.json').then(function(res){
+		deferred.resolve(res.data);
+	});
 
-    return deferred.promise;
+	return deferred.promise;
 }])
 
 .factory('BoxService', function(){
-    /** took me 8 hours -_- i am not a good programmer **/
-    var Box = function(size, turn, set){
-        var box = [],
-        high = 0,
-        low = 0,
-        set = set || [],
-        size = size || 10,
-        turn = turn || 5,
-        setSource, canLoadUp,canLoadDown, up, down;
-        //set = [1,2,3,4,5,6,7,8,9,10]
-        //size = 4
-        //turn = 2,
-        
-        setSource = function(source){
-            return set = source;
-        }
-        
+	/** took me 8 hours -_- i am not a good programmer **/
+	var Box = function(size, turn, set){
+		var box = [],
+		high = 0,
+		low = 0,
+		set = set || [],
+		size = size || 10,
+		turn = turn || 5,
+		setSource, canLoadUp,canLoadDown, up, down;
+		//set = [1,2,3,4,5,6,7,8,9,10]
+		//size = 4
+		//turn = 2,
+		
+		setSource = function(source){
+			return set = source;
+		}
+		
 
-        down = function(){
-            if(!canLoadDown()) return box
-            
-            box = box.concat(set.slice(high, high+turn))
-            
-            high = high+turn > set.length ? set.length : high+turn
-            low = high-size < 0 ? 0 : high-size
+		down = function(){
+			if(!canLoadDown()) return box
+			
+			box = box.concat(set.slice(high, high+turn))
+			
+			high = high+turn > set.length ? set.length : high+turn
+			low = high-size < 0 ? 0 : high-size
 
-            
-            if(box.length > size) box.splice(0, (box.length - size))
-            
-            console.log('low at: '+low)
-            console.log('high at: '+high)
+			
+			if(box.length > size) box.splice(0, (box.length - size))
+			
+			console.log('low at: '+low)
+			console.log('high at: '+high)
 
-            return box
-        }
+			return box
+		}
 
-        up = function(){
-            if(!canLoadUp()) return box
-            
-            box = ( set.slice(low-turn, low) ).concat(box)
+		up = function(){
+			if(!canLoadUp()) return box
+			
+			box = ( set.slice(low-turn, low) ).concat(box)
 
-            low = low-turn < 0 ? 0 : low-turn
-            high = low+size > set.length ? set.length : low+size
+			low = low-turn < 0 ? 0 : low-turn
+			high = low+size > set.length ? set.length : low+size
 
-            if(box.length > size) box.splice(size)
-            
-            console.log('low at: '+low)
-            console.log('high at: '+high)
+			if(box.length > size) box.splice(size)
+			
+			console.log('low at: '+low)
+			console.log('high at: '+high)
 
-            return box
-        }
+			return box
+		}
 
-        canLoadDown = function(){
-            return high < set.length;
-        }
+		canLoadDown = function(){
+			return high < set.length;
+		}
 
-        canLoadUp = function(){
-            return low > 0;
-        }
+		canLoadUp = function(){
+			return low > 0;
+		}
 
 
-        return {
-            down: down,
-            up: up,
-            canLoadUp: canLoadUp,
-            canLoadDown: canLoadDown,
-            setSource: setSource
-        }
-    }
+		return {
+			down: down,
+			up: up,
+			canLoadUp: canLoadUp,
+			canLoadDown: canLoadDown,
+			setSource: setSource
+		}
+	}
 
-    return Box;
+	return Box;
 })
 
 .factory('SurahService',['$http', "$q", function($http, $q){
-    var surahs = {}, redecorateSurah, loadSurah;
+	var surahs = {}, redecorateSurah, loadSurah;
 
-    redecorateSurah = function(surah){
-        var ayahs, arabicAyahs, banglaAyahs;
-        
-        arabicAyahs =_.filter(surah.ayahs, function(ayah, index){
-            return (index+2) % 2; // returns true on 0,2,4,6
-        });
-        
-        banglaAyahs =_.filter(surah.ayahs, function(ayah, index){
-            return !(index % 2); // returns true on 1,3,5,7
-        });
+	redecorateSurah = function(surah){
+		var ayahs, arabicAyahs, banglaAyahs;
+		
+		arabicAyahs =_.filter(surah.ayahs, function(ayah, index){
+			return (index+2) % 2; // returns true on 0,2,4,6
+		});
+		
+		banglaAyahs =_.filter(surah.ayahs, function(ayah, index){
+			return !(index % 2); // returns true on 1,3,5,7
+		});
 
-        ayahs = _.zip(banglaAyahs, arabicAyahs);
-        
-        ayahs = _.map(ayahs, function(ayah, index){
-            return {
-                ar: ayah[0],
-                bn: ayah[1],
-                line: index+1
-            };
-        });
+		ayahs = _.zip(banglaAyahs, arabicAyahs);
+		
+		ayahs = _.map(ayahs, function(ayah, index){
+			return {
+				ar: ayah[0],
+				bn: ayah[1],
+				line: index+1
+			};
+		});
 
-        surah.ayahs = ayahs;
+		surah.ayahs = ayahs;
 
-        return surah;
-    };
+		return surah;
+	};
 
 
-    loadSurah = function(surahId){
-        var deferred = $q.defer(), 
-            url = "/js/res/surahs/" + surahId + ".json";
+	loadSurah = function(surahId){
+		var deferred = $q.defer(), 
+			url = "/js/res/surahs/" + surahId + ".json";
 
-        //if we have a cache do not load it from url
-        if(surahs[surahId]){
-            deferred.resolve(surahs[surahId]);
-            return deferred.promise;            
-        }
+		//if we have a cache do not load it from url
+		if(surahs[surahId]){
+			deferred.resolve(surahs[surahId]);
+			return deferred.promise;			
+		}
 
-        //as we do not have cache lets load it
-        $http.get(url).then(function(res){
-            deferred.resolve(redecorateSurah(res.data));
-        });
+		//as we do not have cache lets load it
+		$http.get(url).then(function(res){
+			deferred.resolve(redecorateSurah(res.data));
+		});
 
-        //cache it
-        deferred.promise.then(function(surah){
-            surahs[surahId] = surah;
-        });
+		//cache it
+		deferred.promise.then(function(surah){
+			surahs[surahId] = surah;
+		});
 
-        return deferred.promise;
-    }
+		return deferred.promise;
+	}
 
-    return {
-        loadSurah: loadSurah
-    };
+	return {
+		loadSurah: loadSurah
+	};
 }])
